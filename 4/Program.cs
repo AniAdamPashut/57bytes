21d4e281fd0bb3969cda949c6da633aa196c0e81
Ben Amiel <minebenking@gmail.com>
2024-05-13 19:17:39 +0300

    https://open.spotify.com/track/67hHJnsk1q9hhnbOhIXG0m?si=fad86bd7a1544e64

--- 4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using _4;
-
-Console.WriteLine("Hello, World!");
-
-
-Board b = new Board();
-Console.WriteLine(b);
-b.Move(Direction.Down);
